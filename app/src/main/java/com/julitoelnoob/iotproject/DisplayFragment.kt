ddd2c855478d12285dbb8837053d7fc90dc6c45a
HecMurillo <xjuliopvp@gmail.com>
2024-02-13 08:47:20 -0700

    se creo el fragmento display

---
+++ app/src/main/java/com/julitoelnoob/iotproject/DisplayFragment.kt
@@ -0,0 +1,22 @@
+package com.julitoelnoob.iotproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class DisplayFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_display, container, false)
+        
+        return view
+    }
+
+}
